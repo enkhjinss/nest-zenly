cc9287b5701a6eb1dcb208bcc76b6b3bef6251c4
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-18 15:58:47 +0800

    ghfd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+const axios = require("axios");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    // const config = {
+    //     method: "get",
+    //     url: "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&avoid=highways&mode=bicycling&key=AIzaSyBr_qJTe-9_TZhGM2Dhwh_NmGrKArMaR-k",
+    //     headers: {},
+    // };
+    axios.get("https://maps.googleapis.com/maps/api/directions/json?origin=37.7680296%2C-122.4375126&destination=side_of_road%3A37.7663444%2C-122.4412006&key=AIzaSyBr_qJTe-9_TZhGM2Dhwh_NmGrKArMaR-k").then((res) => {
+      console.log(res.data);
+    })
+    // axios(config)
+    //     .then(function (response) {
+    //         console.log(JSON.stringify(response.data));
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+
+    return <div>hi</div>;
+};
 
 export default App;
